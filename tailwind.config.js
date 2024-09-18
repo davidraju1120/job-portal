5366c3c971f3ee9c4d6c5e58bd8d5b82ecc00612
“Hammad <contact.hammadk@gmail.com>
2024-09-18 08:29:57 +0530

    tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
-    content: [ "./src/**/*.{js,jsx,ts,tsx}"],
+    // content: [ "./src/**/*.{js,jsx,ts,tsx}"],
+	content: [
+		'./src/**/*.html',
+		'./src/**/*.{js,jsx,ts,tsx}',
+		'./public/index.html',
+	  ],
+	
   theme: {
   	extend: {
 		
